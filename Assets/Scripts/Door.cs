467739a39fdbd3079a8643556b4e28e52ffddd08
Alberto <alberto@xamin.it>
2020-02-01 12:30:11 +0100

    interactable door

---
+++ Assets/Scripts/Door.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour, Interactable
+{
+    private bool opened;
+    [SerializeField]private Animator doorAnimator;
+    public void Interact()
+    {
+        opened = !opened;
+        doorAnimator.SetBool("open", opened);
+    }
+}
