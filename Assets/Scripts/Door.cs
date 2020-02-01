b3c121395ed2eaa726ebda3c2a92f8f1fb1143ba
Alberto <alberto@xamin.it>
2020-02-01 16:18:27 +0100

    Door sound

--- Assets/Scripts/Door.cs
+++ Assets/Scripts/Door.cs
@@ -5,10 +5,14 @@ using UnityEngine;
 public class Door : Interactable
 {
     private bool opened;
-    [SerializeField]private Animator doorAnimator;
+    [SerializeField] private Animator doorAnimator;
+    [SerializeField] AudioClip doorOpen, doorClose;
+
     public override void Interact()
     {
         opened = !opened;
         doorAnimator.SetBool("open", opened);
+        GetComponent<AudioSource>().Stop();
+        GetComponent<AudioSource>().PlayOneShot(opened ? doorOpen : doorClose);
     }
 }
