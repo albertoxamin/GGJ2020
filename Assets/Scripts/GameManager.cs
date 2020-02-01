1baf5cf0899cd47cf24915c52af764a2687cc62f
Alberto Xamin <alberto@xamin.it>
2020-02-01 11:36:14 +0100

    more main menu

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,22 +2,41 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
     public float GivenTime = 500f;
     private float startTime;
-    private void Start()
+    private bool isTimerStarted;
+
+    private void Awake()
     {
+        DontDestroyOnLoad(this);
+        DontDestroyOnLoad(Hud.Instance.gameObject.transform.parent);
+        
+    }
+
+    private void StartTimer()
+    {
+        isTimerStarted = true;
         startTime = Time.time;
     }
 
     void Update()
     {
-        float time = GivenTime - (Time.time - startTime);
-        if (time > 0)
-            Hud.Instance.SetRemainingTimeText(time);
-        else
-            Hud.Instance.SetRemainingTimeText(0);
+        if (isTimerStarted)
+        {
+            float time = GivenTime - (Time.time - startTime);
+            if (time > 0)
+                Hud.Instance.SetRemainingTimeText(time);
+            else
+                Hud.Instance.SetRemainingTimeText(0);
+        }
+    }
+
+    public void PlayGame()
+    {
+        SceneManager.LoadSceneAsync("Scenes/TestLogic");
     }
 }
