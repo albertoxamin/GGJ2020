c316430c0c8b995f509dadbef4b91763155bc570
Alberto Xamin <alberto@xamin.it>
2020-02-01 10:55:47 +0100

    add basic hud

---
+++ Assets/Scripts/Hud.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Random = UnityEngine.Random;
+
+public class Hud : MonoBehaviour
+{
+    public static Hud Instance;
+    [SerializeField] private Text trapsRemainingText, timeRemainingText;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    public void SetTrapText(int done, int total)
+    {
+        trapsRemainingText.text = $"{done:00}/{total}";
+    }
+
+    public void SetRemainingTimeText(float time)
+    {
+        int minutes = (int)time / 60;
+        int seconds = (int)time - minutes * 60;
+        timeRemainingText.text = $"{minutes:00}:{seconds:00}";
+    }
+}
