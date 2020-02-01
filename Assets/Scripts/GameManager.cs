d7a398fa72eba0f788b79c8b1d3660384a695b75
Alberto Xamin <alberto@xamin.it>
2020-02-01 11:21:01 +0100

    main menu

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public float GivenTime = 500f;
+    private float startTime;
+    private void Start()
+    {
+        startTime = Time.time;
+    }
+
+    void Update()
+    {
+        float time = GivenTime - (Time.time - startTime);
+        if (time > 0)
+            Hud.Instance.SetRemainingTimeText(time);
+        else
+            Hud.Instance.SetRemainingTimeText(0);
+    }
+}
