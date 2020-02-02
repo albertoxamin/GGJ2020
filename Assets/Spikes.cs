61e9e4d0e2834931b6cbbf2f4fc04abf10de1d65
matteo <32160291+Matteounitn@users.noreply.github.com>
2020-02-02 10:55:45 +0100

    Spiedini di Goblin

---
+++ Assets/Spikes.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spikes : Interactable
+{
+    private bool broken = true;
+    private bool trigger = false;
+    [SerializeField] private Animator spikeAnimator;
+    [SerializeField] AudioClip spikeRepair, spikeTrigger;
+
+    public override void Interact()
+    {
+        
+        if (broken)
+        {
+            spikeAnimator.SetBool("broken", false);
+            spikeAnimator.SetBool("trigger", false);
+            trigger = false;
+            broken = false;
+            GetComponent<AudioSource>().Stop();
+            GetComponent<AudioSource>().PlayOneShot(spikeRepair);
+            InteractionText = "";
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log(broken+"-"+trigger);
+        if (!broken && !trigger)
+        {
+            spikeAnimator.SetBool("broken", false);
+            spikeAnimator.SetBool("trigger", true);
+            GetComponent<AudioSource>().Stop();
+            GetComponent<AudioSource>().PlayOneShot(spikeTrigger);
+            trigger = true;
+            broken = true;
+            spikeAnimator.SetBool("broken", true);
+            spikeAnimator.SetBool("trigger", true);
+            InteractionText = "Press E to Repair";
+        }
+    }
+}
