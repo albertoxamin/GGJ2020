32b20ee318002c6f1bb4187d03732a57a4afe587
matteo <32160291+Matteounitn@users.noreply.github.com>
2020-02-02 16:47:16 +0100

    Summon sound and microfixes

--- Assets/Scripts/SummonTrap.cs
+++ Assets/Scripts/SummonTrap.cs
@@ -7,10 +7,22 @@ public class SummonTrap : Interactable
     private bool m_Broken = true;
     [SerializeField] private Animator devilAnimator;
     public Transform demon;
+    [SerializeField] AudioClip demonTriggerOne, demonTriggerTwo,demonTriggerThunder;
     private void Start()
     {
         GameManager.Instance.traps++;
         base.Start();
+    }
+    IEnumerator waitAndPlay()
+    {
+        AudioSource audio = GetComponent<AudioSource>();
+        audio.Stop();
+        audio.PlayOneShot(demonTriggerOne);
+        yield return new WaitForSeconds(0.3f);
+        audio.PlayOneShot(demonTriggerTwo);
+        yield return new WaitForSeconds(0.4f);
+        audio.PlayOneShot(demonTriggerThunder);
+        
     }
     public override void Interact()
     {
@@ -21,7 +33,12 @@ public class SummonTrap : Interactable
             InteractionText = "";
             demon.transform.localPosition = Vector3.up * 0.3f;
             GameManager.Instance.notBrokenTraps += 1;
+            StartCoroutine(waitAndPlay());
+            
+            
+            
+        }
         }
         
     }
-}
+
