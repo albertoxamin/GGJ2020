ad06f4ceca7d9fa2c5fdb09d5a033afc3d6f83be
matteo <32160291+Matteounitn@users.noreply.github.com>
2020-02-02 13:28:10 +0100

    New axetrap

---
+++ Assets/Scripts/Axe.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+public class Axe : Interactable
+{
+    private bool broken = true;
+    private bool trigger = false; 
+    [SerializeField] private Animator axeAnimator;
+    [SerializeField] AudioClip axeRepair, axeTrigger;
+
+    public override void Interact()
+    {
+        
+        if (broken)
+        {
+            axeAnimator.SetBool("broken", false);
+            axeAnimator.SetBool("trigger", false);
+            trigger = false;
+            broken = false;
+            GetComponent<AudioSource>().Stop();
+            GetComponent<AudioSource>().PlayOneShot(axeRepair);
+            InteractionText = "";
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log(broken+"-"+trigger);
+        if (!broken && !trigger)
+        {
+            axeAnimator.SetBool("broken", false);
+            axeAnimator.SetBool("trigger", true);
+            GetComponent<AudioSource>().Stop();
+            GetComponent<AudioSource>().PlayOneShot(axeTrigger);
+            trigger = true;
+            broken = true;
+            axeAnimator.SetBool("broken", true);
+            axeAnimator.SetBool("trigger", true);
+            InteractionText = "Press E to Repair";
+        }
+    }
+}
