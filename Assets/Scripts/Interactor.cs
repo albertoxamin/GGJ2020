7edd886118bf796cfb6a015ea2d45fd1f870d8a7
Alberto <alberto@xamin.it>
2020-02-02 10:27:09 +0100

    Lancia e truffa

--- Assets/Scripts/Interactor.cs
+++ Assets/Scripts/Interactor.cs
@@ -2,17 +2,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+
 [RequireComponent(typeof(Camera))]
 public class Interactor : MonoBehaviour
 {
     private Camera playerCamera;
-    [SerializeField]private Animator Animator;
+    [SerializeField] private Animator Animator;
+    public Transform HoldPosition;
+    private Transform heldObject;
 
     private void Awake()
     {
         playerCamera = GetComponent<Camera>();
     }
-    
+
     void Update()
     {
         RaycastHit hit;
@@ -27,11 +30,34 @@ public class Interactor : MonoBehaviour
                 interactable.Interact();
             }
         }
+        else if (hit.collider && hit.rigidbody && !heldObject)
+        {
+            Hud.Instance.SetInteractionText("Press E to grab");
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                heldObject = hit.transform;
+                hit.rigidbody.isKinematic = true;
+                heldObject.SetParent(HoldPosition);
+                heldObject.localPosition = Vector3.zero;
+                heldObject.localRotation = Quaternion.identity;
+            }
+        }
         else
         {
             Hud.Instance.SetInteractionText("");
         }
+
+        if (heldObject)
+        {
+            if (Input.GetKeyDown(KeyCode.Q))
+            {
+                heldObject.GetComponent<Rigidbody>().isKinematic = false;
+                heldObject.GetComponent<Rigidbody>().AddForce((transform.forward+transform.up)*500f);
+                heldObject.SetParent(null);
+                heldObject = null;
+            }
+        }
+
         Animator.SetFloat("MovementSpeed", transform.parent.GetComponent<Rigidbody>().velocity.magnitude);
     }
-    
 }
