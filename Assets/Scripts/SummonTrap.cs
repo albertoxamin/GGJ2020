e34b9743724d996c3d9ac5a39664ba8167118ffe
Alberto <alberto@xamin.it>
2020-02-02 15:43:57 +0100

    fixe

---
+++ Assets/Scripts/SummonTrap.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SummonTrap : Interactable
+{
+    private bool m_Broken = true;
+    [SerializeField] private Animator spikeAnimator;
+    [SerializeField] AudioClip spikeRepair, spikeTrigger;
+
+    private void Start()
+    {
+        GameManager.Instance.traps++;
+        base.Start();
+    }
+    public override void Interact()
+    {
+        if (m_Broken)
+        {
+            spikeAnimator.SetBool("broken", false);
+            m_Broken = false;
+            InteractionText = "";
+            GetComponent<AudioSource>().Stop();
+            GetComponent<AudioSource>().PlayOneShot(spikeRepair);
+            GameManager.Instance.notBrokenTraps += 1;
+        }
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!m_Broken)
+        {
+            spikeAnimator.SetTrigger("active");
+            GetComponent<AudioSource>().Stop();
+            GetComponent<AudioSource>().PlayOneShot(spikeTrigger);
+        }
+    }
+}
